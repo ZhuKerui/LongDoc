4a14c247dbfbff8f369dd522ced9533984720a74
ZhuKerui <keruiz2@illinois.edu>
2024-03-05 13:57:37 -0600

    temp save

--- sh/start_llm.sh
+++ sh/start_llm.sh
@@ -1,4 +1,5 @@
-export CUDA_VISIBLE_DEVICES=1,2
-MODEL=meta-llama/Llama-2-7b-hf
+export CUDA_VISIBLE_DEVICES=1
+# MODEL=meta-llama/Llama-2-7b-chat-hf
+MODEL=mistralai/Mistral-7B-Instruct-v0.2
 
-python -m vllm.entrypoints.openai.api_server --model $MODEL --tensor-parallel-size 2
+python -m vllm.entrypoints.openai.api_server --model $MODEL --tensor-parallel-size 1
