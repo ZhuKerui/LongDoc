dff177b395b2be8e077eb7970e51bc8a6c06b2b6
ZhuKerui <keruiz2@illinois.edu>
2024-06-20 00:16:04 -0500

    temp save

--- sh/start_llm.sh
+++ sh/start_llm.sh
@@ -1,5 +1,6 @@
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=1,2
 # MODEL=meta-llama/Llama-2-7b-chat-hf
-MODEL=mistralai/Mistral-7B-Instruct-v0.2
+# MODEL=mistralai/Mistral-7B-Instruct-v0.2
+MODEL=microsoft/Phi-3-mini-128k-instruct
 
-python -m vllm.entrypoints.openai.api_server --model $MODEL --tensor-parallel-size 1 --dtype bfloat16
+python -m vllm.entrypoints.openai.api_server --model $MODEL --tensor-parallel-size 2 --dtype bfloat16
