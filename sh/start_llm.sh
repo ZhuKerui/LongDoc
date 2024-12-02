f80cc7e90c69b067f5b31f92cf476b89628935bd
ZhuKerui <keruiz2@illinois.edu>
2024-12-02 10:04:24 -0600

    temp save

--- sh/start_llm.sh
+++ sh/start_llm.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=2
 # MODEL=meta-llama/Llama-2-7b-chat-hf
 # MODEL=mistralai/Mistral-7B-Instruct-v0.3
 # MODEL=microsoft/Phi-3-mini-128k-instruct
